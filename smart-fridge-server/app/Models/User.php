2fc3c6e0a08b9966c06384747d70a706890c2843
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-13 16:12:58 +0300

    Backend - Adding JWTSubject implementation to User model

--- smart-fridge-server/app/Models/User.php
+++ smart-fridge-server/app/Models/User.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -18,9 +19,11 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
         'email',
         'password',
+        'role'
     ];
 
     /**
@@ -38,6 +41,15 @@ class User extends Authenticatable
      *
      * @return array<string, string>
      */
+
+     public function getJWTIdentifier(){
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims(){
+        return [];
+    }
+
     protected function casts(): array
     {
         return [
