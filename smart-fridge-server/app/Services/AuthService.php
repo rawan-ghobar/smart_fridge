a48bac4054e4f3c45e871328e63753db6e0321d4
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-13 18:47:30 +0300

    Backend - Adding logout method for token invalidation

--- smart-fridge-server/app/Services/AuthService.php
+++ smart-fridge-server/app/Services/AuthService.php
@@ -5,6 +5,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use App\Traits\ResponseTrait;
 use App\Http\Requests\LoginRequest;
+use App\Http\Requests\SignupRequest;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth;
@@ -30,5 +31,34 @@ class AuthService
             'user'  => $user,
             'token' => $token,
         ]);
+    }
+
+    public function signup(SignupRequest $request)
+    {
+        $data = $request->validated();
+        $data['password'] = Hash::make($data['password']);
+
+        $user = User::create($data);
+
+        return $this->successResponse([
+            'message' => 'User registered successfully.',
+            'user'    => $user,
+        ], 201);
+    }
+
+    public function logout(Request $request)
+    {
+        try {
+            if (!$token = JWTAuth::getToken()){
+                return $this->errorResponse('Token not provided.', 400);
+            }
+            JWTAuth::invalidate($token);
+
+            return $this->successResponse(['message' => 'User logged out successfully.']);
+            }
+
+             catch (JWTException $e) {
+                return $this->errorResponse('Failed to logout, please try again.', 500);
+            }
     }
 }
