1c738a51ad7d649237a0a8581477797b958f5bc2
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-13 19:05:19 +0300

    Backend - Adding login route

--- smart-fridge-server/routes/api.php
+++ smart-fridge-server/routes/api.php
@@ -6,6 +6,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('v0.1')->group(function () {
 
+    Route::prefix('user')->group(function () {
+            Route::post('/logout', [AuthController::class, 'logout']);
+    });
+
     Route::prefix('guest')->group(function () {
         Route::post('/login',  [AuthController::class, 'login']);
         Route::post('/signup', [AuthController::class, 'signup']);
