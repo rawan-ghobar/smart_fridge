a84193841993a6ec0837f081ad0c58504c0b164d
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-13 17:48:35 +0300

    Backend - Adding relationships to ShoppingListItem model

--- smart-fridge-server/app/Models/ShoppingListItem.php
+++ smart-fridge-server/app/Models/ShoppingListItem.php
@@ -13,4 +13,9 @@ class ShoppingListItem extends Model
         'shopping_list_id',
         'name'
     ];
+
+    public function shoppingList()
+    {
+        return $this->belongsTo(ShoppingList::class);
+    }
 }
