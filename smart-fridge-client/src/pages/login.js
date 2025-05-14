66dea2e25bc96ab41057751499fe7a6886a5249b
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-14 22:30:16 +0300

    Frontend - Adding color constants

--- smart-fridge-client/src/pages/login.js
+++ smart-fridge-client/src/pages/login.js
@@ -92,6 +92,16 @@ const Login = ({ navigation }) => {
       </View>
     </View>
   );
+  const COLORS = {
+  primary: '#033BB5',
+  background: '#F7F9FB',
+  primaryAccent: '#2A6AE4',
+  secondaryAccent: '#F96E2A',
+  placeholder: '#C0C0C0',
+  white: '#FFFFFF',
+  border: '#E0E0E0',
+};
+
 
 
 
