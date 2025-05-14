52e755b0f8e047fdd7d9f29c99ae34b658493779
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-14 22:04:55 +0300

    Frontend - Adding handleLogin function

--- smart-fridge-client/src/pages/login.js
+++ smart-fridge-client/src/pages/login.js
@@ -11,3 +11,9 @@ const Login = ({ navigation }) => {
   const [passwordVisible, setPasswordVisible] = useState(false);
 
   const togglePasswordVisibility = () => setPasswordVisible(prev => !prev);
+  
+  const handleLogin = async () => {
+  try {
+    const response = await api.post('/guest/login', { email, password }, {
+      headers: { 'Content-Type': 'application/json' },
+    });
