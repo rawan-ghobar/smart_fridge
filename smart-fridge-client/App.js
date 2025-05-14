b13a08840d07d9eecb0881e28b24b756d997a9a6
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-14 22:37:58 +0300

    Frontend - Adding fetchfonts

--- smart-fridge-client/App.js
+++ smart-fridge-client/App.js
@@ -3,3 +3,11 @@ import { Text as DefaultText } from 'react-native';
 import * as Font from 'expo-font';
 import AppLoading from 'expo-app-loading';
 import Routes from './src/routes/routes';
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'Outfit-Regular': require('./assets/fonts/Outfit/static/Outfit-Regular.ttf'),
+    'Outfit-Bold': require('./assets/fonts/Outfit/static/Outfit-Bold.ttf'),
+    'Outfit-SemiBold': require('./assets/fonts/Outfit/static/Outfit-SemiBold.ttf'),
+  });
+};
