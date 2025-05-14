2438f0f7bc8e5a4be2b5f0417451704d9ed0c60e
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-14 22:37:22 +0300

    Frontend - Adding imports for app.js

--- smart-fridge-client/App.js
+++ smart-fridge-client/App.js
@@ -1,20 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import React from 'react';
+import { Text as DefaultText } from 'react-native';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
+import Routes from './src/routes/routes';
