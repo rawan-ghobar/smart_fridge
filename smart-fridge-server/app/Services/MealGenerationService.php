fd16cbdd864b5fd0a7ab062cb21196ec23d7f9f0
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-14 15:59:57 +0300

    Backend - Creating Meal Generation Service

---
+++ smart-fridge-server/app/Services/MealGenerationService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services;
+
+use Prism\Prism\Prism;
+use Prism\Prism\Enums\Provider;
+use App\Schemas\MealGenerationSchema;
+use Illuminate\Support\Collection;
+
+class MealGenerationService
+{
+    
+
+}
