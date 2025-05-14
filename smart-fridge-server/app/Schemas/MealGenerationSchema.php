f53b696429bedfbebfadff4670a6124bcc486538
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-14 16:04:02 +0300

    Backend - creating Prism schema

---
+++ smart-fridge-server/app/Schemas/MealGenerationSchema.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Schemas;
+
+use Prism\Prism\Schema\ObjectSchema;
+use Prism\Prism\Schema\StringSchema;
+
+class MealGenerationSchema
+{
+    public static function createPrismSchema($name, $description, $properties) {
+        $schema = new ObjectSchema(
+            name: $name,
+            description: $description,
+            properties: self::createProperties($properties),
+            requiredFields: array_keys($properties)
+        );
+
+        return $schema;
+
+    }
+}
