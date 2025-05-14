ec015a2d2d3972098a142bda2d33ffc9d6c72363
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-14 17:55:51 +0300

    Backend - Creating Meal Generation Controller and implementing generate with calories controller method

--- smart-fridge-server/app/Http/Controllers/MealGenerationController.php
+++ smart-fridge-server/app/Http/Controllers/MealGenerationController.php
@@ -22,4 +22,18 @@ class MealGenerationController extends Controller
             return ResponseTrait::successResponse($result);
         }
 
+        public function generateMealWithCalorieLimit(Request $request)
+        {
+            $mealType = $request->input('mealType');
+            $fridgeId = $request->input('fridgeId');
+            $usercalories = $request->input('usercalories');
+            $userNotes = $request->input('userNotes');
+
+
+            $items = ItemService::getItems($fridgeId);
+            $result = MealGenerationWithCaloriesService::generateMealWithCalorieLimit($items,$mealType, $usercalories, $userNotes );
+
+            return ResponseTrait::successResponse($result);
+        }
+
 }
