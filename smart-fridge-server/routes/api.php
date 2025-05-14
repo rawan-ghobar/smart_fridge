6f593aa72965b582f539c83b7d3615a232a3dcd6
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-14 15:21:29 +0300

    Backend - deleting unnecessary imports from routes

--- smart-fridge-server/routes/api.php
+++ smart-fridge-server/routes/api.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\FridgeController;
 use App\Http\Controllers\ItemController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v0.1')->group(function () {
