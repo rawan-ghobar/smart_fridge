1af8b4ec2481c6dba71d8d27d1370840628d00dd
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-14 22:03:16 +0300

    Frontend - Creating login component

--- smart-fridge-client/src/pages/login.js
+++ smart-fridge-client/src/pages/login.js
@@ -4,3 +4,8 @@ import { Ionicons } from '@expo/vector-icons';
 import AppText from '../components/textWrapper';
 import api from '../services/api';
 import AsyncStorage from '@react-native-async-storage/async-storage'; 
+
+const Login = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordVisible, setPasswordVisible] = useState(false);
