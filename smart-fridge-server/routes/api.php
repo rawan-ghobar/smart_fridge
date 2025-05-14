9b1c6e9436baba073ad585bca6009dbb070235e5
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-14 07:33:33 +0300

    Backend - Creating fridge connection endpoint

--- smart-fridge-server/routes/api.php
+++ smart-fridge-server/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\FridgeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,6 +11,10 @@ Route::prefix('v0.1')->group(function () {
 
         Route::prefix('user')->group(function () {
                 Route::post('/logout', [AuthController::class, 'logout']);
+        });
+
+        Route::prefix('fridge')->group(function () {
+            Route::post('/connect', [FridgeController::class, 'connect']);
         });
 
     });
