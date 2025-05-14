f3f55ccb447375bd582979f1bfe608c1a4f872fa
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-14 15:16:06 +0300

    Backend - Adding getItem endpoint

--- smart-fridge-server/routes/api.php
+++ smart-fridge-server/routes/api.php
@@ -23,6 +23,7 @@ Route::prefix('v0.1')->group(function () {
 
         Route::prefix('items')->group(function () {
             Route::get('/getitems/{fridgeId}' , [ItemController::class, 'getItems']);
+            Route::get('/getitem/{fridgeId}/{itemId}', [ItemController::class, 'getItem']);
         });
 
     });
