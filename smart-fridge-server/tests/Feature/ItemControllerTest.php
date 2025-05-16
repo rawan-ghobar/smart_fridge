0adec62265e8668fa13f1dc3a5788eedbce725bf
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-16 12:21:52 +0300

    Backend - Renaming delete item test name

--- smart-fridge-server/tests/Feature/ItemControllerTest.php
+++ smart-fridge-server/tests/Feature/ItemControllerTest.php
@@ -71,7 +71,7 @@ class ItemControllerTest extends TestCase
         $this->assertDatabaseHas('fridge_items', ['id' => $item->id, 'name' => 'Yogurt']);
     }
 
-    public function delete_item()
+    public function test_delete_item()
     {
         $fridge = Fridge::factory()->create(['user_id' => $this->user->id]);
         $item = FridgeItem::factory()->create(['fridge_id' => $fridge->id]);
