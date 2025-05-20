2a2f021aa7ab5c182932a2ea63bb06ab2ab77da1
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-20 23:24:30 +0300

    Frontend - Importing COLORS from theme

--- smart-fridge-client/src/features/items/screens/IteListScreen.js
+++ smart-fridge-client/src/features/items/screens/IteListScreen.js
@@ -3,3 +3,4 @@ import { SafeAreaView, View, Text, TouchableOpacity, FlatList, ActivityIndicator
 import { Ionicons } from '@expo/vector-icons';
 import { useItemList } from '../hooks/useItemList';
 import ItemCard from '../components/ItemCard';
+import COLORS from '../../../theme/colors';
