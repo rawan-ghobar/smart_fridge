75a120eb284822384b3e5b1ab70792cb7388780d
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-20 23:25:12 +0300

    Frontend - Creating ItemListScreen and initializing item list hook

--- smart-fridge-client/src/features/items/screens/IteListScreen.js
+++ smart-fridge-client/src/features/items/screens/IteListScreen.js
@@ -5,3 +5,5 @@ import { useItemList } from '../hooks/useItemList';
 import ItemCard from '../components/ItemCard';
 import COLORS from '../../../theme/colors';
 import typography from '../../../theme/typography';
+const ItemListScreen = ({ navigation }) => {
+  const { items, loading, showAddOptions, handleDeleteItem } = useItemList(navigation);
