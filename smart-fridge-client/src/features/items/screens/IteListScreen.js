4ae022f2afae48191eae8529f326ff6dae91238d
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-20 23:25:47 +0300

    Frontend - Conditionally rendering loader or item list

--- smart-fridge-client/src/features/items/screens/IteListScreen.js
+++ smart-fridge-client/src/features/items/screens/IteListScreen.js
@@ -17,3 +17,19 @@ return (
         <Text style={styles.headerTitle}>What's Inside?</Text>
       </View>
     </View>
+{loading ? (
+  <ActivityIndicator size="large" color={COLORS.primaryAccent} style={{ marginTop: 50 }} />
+) : (
+  <FlatList
+    data={items}
+    renderItem={({ item }) => (
+      <ItemCard item={item} onLongPress={() => handleDeleteItem(item.id)} />
+    )}
+    keyExtractor={item => item.id.toString()}
+    contentContainerStyle={styles.listContent}
+    showsVerticalScrollIndicator={false}
+    ListEmptyComponent={
+      <Text style={{ textAlign: 'center', marginTop: 20 }}>No items found.</Text>
+    }
+  />
+)}
