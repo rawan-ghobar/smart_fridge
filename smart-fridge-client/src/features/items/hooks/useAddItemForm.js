9fdd82da466df9d433169b90a8fd18c238df7c5a
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-20 22:06:29 +0300

    Frontend - Importing moment for date formatting

--- smart-fridge-client/src/features/items/hooks/useAddItemForm.js
+++ smart-fridge-client/src/features/items/hooks/useAddItemForm.js
@@ -1,3 +1,4 @@
 import { useState, useEffect } from 'react';
 import { Alert } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import moment from 'moment';
