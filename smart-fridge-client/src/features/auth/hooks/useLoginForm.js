1b55e0a0323188b946850b71b0cac51485acebee
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-20 17:42:56 +0300

    Frontend - Adding state for email, password, and password visibility

--- smart-fridge-client/src/features/auth/hooks/useLoginForm.js
+++ smart-fridge-client/src/features/auth/hooks/useLoginForm.js
@@ -4,3 +4,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import api from '../../../services/api';
 
 export const useLoginForm = (navigation) => {
+    const [email, setEmail] = useState('');
+const [password, setPassword] = useState('');
+const [passwordVisible, setPasswordVisible] = useState(false);
+
