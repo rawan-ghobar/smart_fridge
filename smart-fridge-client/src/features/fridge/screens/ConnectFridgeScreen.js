251d27ec90ada516536194e47f83decff878cb1e
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-20 18:28:30 +0300

    Frontend - Importing color constants from theme

--- smart-fridge-client/src/features/fridge/screens/ConnectFridgeScreen.js
+++ smart-fridge-client/src/features/fridge/screens/ConnectFridgeScreen.js
@@ -3,3 +3,4 @@ import AppText from '../../../components/textWrapper';
 import FridgeCodeInput from '../components/FridgeCodeInput';
 import PasswordInput from '../../auth/components/PasswordInput';
 import { useConnectFridgeForm } from '../hooks/useConnectFridgeForm';
+import COLORS from '../../../theme/colors';
