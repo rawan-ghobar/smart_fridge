b22f5d1e5849f8f9312e390bff69b72fbf1801c5
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-20 23:02:14 +0300

    Frontend - Importing useIsFocused from react-navigation

--- smart-fridge-client/src/features/items/hooks/useItemList.js
+++ smart-fridge-client/src/features/items/hooks/useItemList.js
@@ -2,3 +2,4 @@ import { useState, useEffect } from 'react';
 import { Alert, Platform, ActionSheetIOS } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import * as ImagePicker from 'expo-image-picker';
+import { useIsFocused } from '@react-navigation/native';
