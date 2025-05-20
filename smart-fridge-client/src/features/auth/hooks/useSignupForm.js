37bf2cb03167bc4573d3aba619d23b7348973edf
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-20 17:50:28 +0300

    Frontend - Adding input state for signup form fields

--- smart-fridge-client/src/features/auth/hooks/useSignupForm.js
+++ smart-fridge-client/src/features/auth/hooks/useSignupForm.js
@@ -2,3 +2,8 @@ import { useState } from 'react';
 import { Alert } from 'react-native';
 import api from '../../../services/api';
 export const useSignupForm = (navigation) => {
+const [firstName, setFirstName] = useState('');
+const [lastName, setLastName] = useState('');
+const [email, setEmail] = useState('');
+const [password, setPassword] = useState('');
+const [confirmPassword, setConfirmPassword] = useState('');
