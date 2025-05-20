943668c20c15b4307994f5f1daaf5bcbf87efc29
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-20 22:06:42 +0300

    Frontend - Importing ImagePicker from expo

--- smart-fridge-client/src/features/items/hooks/useAddItemForm.js
+++ smart-fridge-client/src/features/items/hooks/useAddItemForm.js
@@ -2,3 +2,4 @@ import { useState, useEffect } from 'react';
 import { Alert } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import moment from 'moment';
+import * as ImagePicker from 'expo-image-picker';
