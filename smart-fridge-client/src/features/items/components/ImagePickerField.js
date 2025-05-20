fdd52cdae688d6c7adaa646832322b5b92ba9dce
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-20 21:51:30 +0300

    Frontend - Styling label with spacing and primary color

--- smart-fridge-client/src/features/items/components/ImagePickerField.js
+++ smart-fridge-client/src/features/items/components/ImagePickerField.js
@@ -19,3 +19,18 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     width: '100%',
   },
+  label: {
+    marginBottom: 6,
+    fontSize: 16,
+    color: COLORS.primary,
+  },
+  imageBox: {
+    backgroundColor: COLORS.white,
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: COLORS.border,
+    padding: 12,
+    height: 120,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
