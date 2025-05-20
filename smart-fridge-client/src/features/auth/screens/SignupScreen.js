273fa9dc71a612fa5b57470da17e71f962f8b585
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-20 18:12:25 +0300

    Frontend - Importing useSignupForm custom hook

--- smart-fridge-client/src/features/auth/screens/SignupScreen.js
+++ smart-fridge-client/src/features/auth/screens/SignupScreen.js
@@ -2,3 +2,4 @@ import { View, TouchableOpacity, StyleSheet, Image } from 'react-native';
 import AppText from '../../../components/textWrapper';
 import EmailInput from '../components/EmailInput';
 import PasswordInput from '../components/PasswordInput';
+import { useSignupForm } from '../hooks/useSignupForm';
