204ebe2953e8a0cc131684f68ed7267f5e764b06
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-20 23:23:37 +0300

    Frontend - Importing useItemList hook for item management

--- smart-fridge-client/src/features/items/screens/IteListScreen.js
+++ smart-fridge-client/src/features/items/screens/IteListScreen.js
@@ -1,3 +1,4 @@
 import React from 'react';
 import { SafeAreaView, View, Text, TouchableOpacity, FlatList, ActivityIndicator, StyleSheet } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import { useItemList } from '../hooks/useItemList';
