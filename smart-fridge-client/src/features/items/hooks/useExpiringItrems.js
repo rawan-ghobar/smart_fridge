332baa1901663e42c8ff0a6948b2c3043d0fd299
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-21 01:55:57 +0300

    Frontend - Importing Alert from react-native

--- smart-fridge-client/src/features/items/hooks/useExpiringItrems.js
+++ smart-fridge-client/src/features/items/hooks/useExpiringItrems.js
@@ -1,3 +1,4 @@
 import { useState, useEffect } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import api from '../../../services/api';
+import { Alert } from 'react-native';
