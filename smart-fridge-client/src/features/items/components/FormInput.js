2be238b31502a14cfcce28c4456272fe6b112209
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-20 21:45:20 +0300

    Frontend - Creating FormInput functional component

--- smart-fridge-client/src/features/items/components/FormInput.js
+++ smart-fridge-client/src/features/items/components/FormInput.js
@@ -1,2 +1,3 @@
 import { View, Text, TextInput, StyleSheet } from 'react-native';
 import COLORS from '../../../theme/colors';
+const FormInput = ({ label, value, onChangeText, placeholder, keyboardType = 'default' }) => {
