a45f842ba607f38593e19d352ed1f8f021ea22f9
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-20 21:45:33 +0300

    Frontend - Rendering label and styled TextInput in FormInput

--- smart-fridge-client/src/features/items/components/FormInput.js
+++ smart-fridge-client/src/features/items/components/FormInput.js
@@ -1,3 +1,16 @@
 import { View, Text, TextInput, StyleSheet } from 'react-native';
 import COLORS from '../../../theme/colors';
 const FormInput = ({ label, value, onChangeText, placeholder, keyboardType = 'default' }) => {
+return (
+  <View style={styles.container}>
+    <Text style={styles.label}>{label}</Text>
+    <TextInput
+      style={styles.input}
+      placeholder={placeholder}
+      placeholderTextColor={COLORS.placeholder}
+      value={value}
+      onChangeText={onChangeText}
+      keyboardType={keyboardType}
+    />
+  </View>
+);
