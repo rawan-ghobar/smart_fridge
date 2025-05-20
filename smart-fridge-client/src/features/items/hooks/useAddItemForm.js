d89eab8dd9056d8b5b70d92f169da3d0e1a59199
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-20 22:07:31 +0300

    Frontend - Adding state for item name, quantity, unit, and calories

--- smart-fridge-client/src/features/items/hooks/useAddItemForm.js
+++ smart-fridge-client/src/features/items/hooks/useAddItemForm.js
@@ -5,3 +5,7 @@ import moment from 'moment';
 import * as ImagePicker from 'expo-image-picker';
 import api from '../../../services/api';
 export const useAddItemForm = (route, navigation) => {
+const [name, setName] = useState('');
+const [quantity, setQuantity] = useState('');
+const [unit, setUnit] = useState('');
+const [calories, setCalories] = useState('');
