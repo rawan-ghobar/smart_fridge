108575cf6abb514a13b2ddec452e0174bb09bc57
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-20 23:12:47 +0300

    Frontend - Importing core react native components for AddItemScreen

--- smart-fridge-client/src/features/items/screens/AddItemScreen.js
+++ smart-fridge-client/src/features/items/screens/AddItemScreen.js
@@ -1,1 +1,2 @@
 import React from 'react';
+import { SafeAreaView, ScrollView, View, Text, TouchableOpacity, KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard, Platform, ActivityIndicator, StyleSheet } from 'react-native';
