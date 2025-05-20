5fe23495ccc80998393ca65332394457e682f394
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-20 18:01:28 +0300

    Frontend - Creating LoginScreen and initializing login form hook

--- smart-fridge-client/src/features/auth/screens/LoginScreen.js
+++ smart-fridge-client/src/features/auth/screens/LoginScreen.js
@@ -4,3 +4,11 @@ import EmailInput from '../components/EmailInput';
 import PasswordInput from '../components/PasswordInput';
 import { useLoginForm } from '../hooks/useLoginForm';
 import COLORS from '../../../theme/colors';
+const LoginScreen = ({ navigation }) => {
+  const {
+    email, setEmail,
+    password, setPassword,
+    passwordVisible,
+    togglePasswordVisibility,
+    handleLogin,
+  } = useLoginForm(navigation);
