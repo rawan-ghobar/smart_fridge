d475cc5ef6b12e1a7966bb0fcc09b7b2f09ab98d
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-20 21:57:16 +0300

    Frontend - Declaring ItemCard component with props

--- smart-fridge-client/src/features/items/components/ItemCard.js
+++ smart-fridge-client/src/features/items/components/ItemCard.js
@@ -1,3 +1,4 @@
 import React from 'react';
 import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native';
 import COLORS from '../../../theme/colors';
+const ItemCard = ({ item, onLongPress }) => {
