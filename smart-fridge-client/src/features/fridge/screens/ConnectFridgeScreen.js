12ea40aac20ee7e0191dd98ccc7a8bb9c0aed4a5
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-20 18:28:20 +0300

    Frontend - Importing useConnectFridgeForm custom hook

--- smart-fridge-client/src/features/fridge/screens/ConnectFridgeScreen.js
+++ smart-fridge-client/src/features/fridge/screens/ConnectFridgeScreen.js
@@ -2,3 +2,4 @@ import { View, TouchableOpacity, StyleSheet, Image } from 'react-native';
 import AppText from '../../../components/textWrapper';
 import FridgeCodeInput from '../components/FridgeCodeInput';
 import PasswordInput from '../../auth/components/PasswordInput';
+import { useConnectFridgeForm } from '../hooks/useConnectFridgeForm';
