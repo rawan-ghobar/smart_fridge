9b47b3beeeb5430015c00dfdc04528fb15d19e06
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-20 18:26:47 +0300

    Frontend - Importing PasswordInput from auth module

--- smart-fridge-client/src/features/fridge/screens/ConnectFridgeScreen.js
+++ smart-fridge-client/src/features/fridge/screens/ConnectFridgeScreen.js
@@ -1,3 +1,4 @@
 import { View, TouchableOpacity, StyleSheet, Image } from 'react-native';
 import AppText from '../../../components/textWrapper';
 import FridgeCodeInput from '../components/FridgeCodeInput';
+import PasswordInput from '../../auth/components/PasswordInput';
