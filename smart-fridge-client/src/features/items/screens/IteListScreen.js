29e8a1da8779ee6d14db953f51568862140a70ff
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-20 23:24:16 +0300

    Frontend - Importing ItemCard component for rendering items

--- smart-fridge-client/src/features/items/screens/IteListScreen.js
+++ smart-fridge-client/src/features/items/screens/IteListScreen.js
@@ -2,3 +2,4 @@ import React from 'react';
 import { SafeAreaView, View, Text, TouchableOpacity, FlatList, ActivityIndicator, StyleSheet } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { useItemList } from '../hooks/useItemList';
+import ItemCard from '../components/ItemCard';
