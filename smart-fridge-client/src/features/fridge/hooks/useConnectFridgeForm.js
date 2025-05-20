08d367674841d9975f79a9f139dd1ee52052390d
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-20 18:20:42 +0300

    Frontend - Adding state for fridge password

--- smart-fridge-client/src/features/fridge/hooks/useConnectFridgeForm.js
+++ smart-fridge-client/src/features/fridge/hooks/useConnectFridgeForm.js
@@ -4,3 +4,4 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import api from '../../../services/api';
 export const useConnectFridgeForm = (navigation) => {
 const [code, setCode] = useState('');
+const [password, setPassword] = useState('');
