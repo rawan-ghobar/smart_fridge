88d4ca5295380aa9aca259608bd8df97ada89a7d
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-20 23:27:56 +0300

    Frontend - Styling header title with typography

--- smart-fridge-client/src/features/items/screens/IteListScreen.js
+++ smart-fridge-client/src/features/items/screens/IteListScreen.js
@@ -41,3 +41,10 @@ const styles = StyleSheet.create({
   safeArea: { flex: 1, backgroundColor: COLORS.background },
   headerWrapper: { paddingHorizontal: 20, paddingVertical: 10, marginTop: 30 },
   header: { flexDirection: 'row', alignItems: 'center', position: 'relative' },
+  headerTitle: {
+    ...typography.header,
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    textAlign: 'center',
+  },
