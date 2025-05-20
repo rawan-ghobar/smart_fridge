6a25ebf6547d8b74ea01df6b6928f5ae9d4eaa16
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-20 23:13:36 +0300

    Frontend - Importing moment for date formatting

--- smart-fridge-client/src/features/items/screens/AddItemScreen.js
+++ smart-fridge-client/src/features/items/screens/AddItemScreen.js
@@ -2,3 +2,4 @@ import React from 'react';
 import { SafeAreaView, ScrollView, View, Text, TouchableOpacity, KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard, Platform, ActivityIndicator, StyleSheet } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import DateTimePicker from '@react-native-community/datetimepicker';
+import moment from 'moment';
