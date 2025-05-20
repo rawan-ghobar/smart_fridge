cb8ba1b755dc73f1c09f22283ed82fe39fca3871
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-20 18:26:33 +0300

    Frontend - Importing FridgeCodeInput component

--- smart-fridge-client/src/features/fridge/screens/ConnectFridgeScreen.js
+++ smart-fridge-client/src/features/fridge/screens/ConnectFridgeScreen.js
@@ -1,2 +1,3 @@
 import { View, TouchableOpacity, StyleSheet, Image } from 'react-native';
 import AppText from '../../../components/textWrapper';
+import FridgeCodeInput from '../components/FridgeCodeInput';
