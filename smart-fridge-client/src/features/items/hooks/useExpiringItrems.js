f6e8e8acc0a900c031ee2ba0e9a28dc21810cd8f
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-21 01:56:19 +0300

    Frontend - Declaring useExpiringItems custom hook

--- smart-fridge-client/src/features/items/hooks/useExpiringItrems.js
+++ smart-fridge-client/src/features/items/hooks/useExpiringItrems.js
@@ -2,3 +2,4 @@ import { useState, useEffect } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import api from '../../../services/api';
 import { Alert } from 'react-native';
+const useExpiringItems = () => {
