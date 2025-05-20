653bcda773a997a2c20fd9f3013d4b670bbbe05f
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-20 17:42:28 +0300

    Frontend - Creating useLoginForm custom hook

--- smart-fridge-client/src/features/auth/hooks/useLoginForm.js
+++ smart-fridge-client/src/features/auth/hooks/useLoginForm.js
@@ -2,3 +2,5 @@ import { useState } from 'react';
 import { Alert } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import api from '../../../services/api';
+
+export const useLoginForm = (navigation) => {
