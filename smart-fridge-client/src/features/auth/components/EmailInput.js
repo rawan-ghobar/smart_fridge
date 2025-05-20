bba7b5eefe1f1de1b4d97504548eee01a125b0a5
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-20 17:18:45 +0300

    Frontend - Defining  EmailInput component with label and text input

--- smart-fridge-client/src/features/auth/components/EmailInput.js
+++ smart-fridge-client/src/features/auth/components/EmailInput.js
@@ -1,3 +1,20 @@
 import { View, TextInput, StyleSheet } from 'react-native';
 import AppText from '../../../components/textWrapper';
 import COLORS from '../../../theme/colors';
+
+const EmailInput = ({ value, onChangeText, label = 'Email' }) => {
+  return (
+    <View style={styles.inputGroup}>
+      <AppText style={styles.label}>{label}</AppText>
+      <TextInput
+        placeholder="example@mail.com"
+        placeholderTextColor={COLORS.placeholder}
+        keyboardType="email-address"
+        autoCapitalize="none"
+        value={value}
+        onChangeText={onChangeText}
+        style={styles.input}
+      />
+    </View>
+  );
+};
