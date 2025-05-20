456491b76c22890e098fff5bcbeb8acec6fd3e40
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-20 18:28:46 +0300

    Frontend - Creating ConnectFridgeScreen and initializing form hook

--- smart-fridge-client/src/features/fridge/screens/ConnectFridgeScreen.js
+++ smart-fridge-client/src/features/fridge/screens/ConnectFridgeScreen.js
@@ -4,3 +4,10 @@ import FridgeCodeInput from '../components/FridgeCodeInput';
 import PasswordInput from '../../auth/components/PasswordInput';
 import { useConnectFridgeForm } from '../hooks/useConnectFridgeForm';
 import COLORS from '../../../theme/colors';
+const ConnectFridgeScreen = ({ navigation }) => {
+  const {
+    code, setCode,
+    password, setPassword,
+    passwordVisible, togglePasswordVisibility,
+    handleConnect,
+  } = useConnectFridgeForm(navigation);
