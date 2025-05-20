52bda5242cad83f1a0ec715f32624b6f577f3991
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-20 21:49:48 +0300

    Frontend - Defining ImagePickerField component with props

--- smart-fridge-client/src/features/items/components/ImagePickerField.js
+++ smart-fridge-client/src/features/items/components/ImagePickerField.js
@@ -1,3 +1,4 @@
 import React from 'react';
 import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native';
 import COLORS from '../../../theme/colors';
+const ImagePickerField = ({ label = 'Image (optional)', imageUri, onPick }) => {
