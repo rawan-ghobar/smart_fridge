56e0a649f3f2ae0a422cbdc9a481120e3f022999
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-20 21:57:37 +0300

    Frontend - Rendering TouchableOpacity as item wrapper

--- smart-fridge-client/src/features/items/components/ItemCard.js
+++ smart-fridge-client/src/features/items/components/ItemCard.js
@@ -2,3 +2,5 @@ import React from 'react';
 import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native';
 import COLORS from '../../../theme/colors';
 const ItemCard = ({ item, onLongPress }) => {
+  return (
+    <TouchableOpacity onLongPress={onLongPress} style={styles.itemCard}>
