62ee35965a7555d11a8e697e2c4834ba76469b8e
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-20 17:28:45 +0300

    Frontend - Defining PasswordInput component with props

--- smart-fridge-client/src/features/auth/components/PasswordInput.js
+++ smart-fridge-client/src/features/auth/components/PasswordInput.js
@@ -2,3 +2,11 @@ import { View, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import AppText from '../../../components/textWrapper';
 import COLORS from '../../../theme/colors';
+
+const PasswordInput = ({
+  value,
+  onChangeText,
+  visible,
+  toggleVisibility,
+  label = 'Password',
+}) => {
