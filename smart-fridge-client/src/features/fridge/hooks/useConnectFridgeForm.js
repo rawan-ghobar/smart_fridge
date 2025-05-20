c7943ec0f74271cc82e13d41ba2595bc0db25830
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-20 18:20:29 +0300

    Frontend - Adding state for fridge connection code

--- smart-fridge-client/src/features/fridge/hooks/useConnectFridgeForm.js
+++ smart-fridge-client/src/features/fridge/hooks/useConnectFridgeForm.js
@@ -3,3 +3,4 @@ import { Alert } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import api from '../../../services/api';
 export const useConnectFridgeForm = (navigation) => {
+const [code, setCode] = useState('');
