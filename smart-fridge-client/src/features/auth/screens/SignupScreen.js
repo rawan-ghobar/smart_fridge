ec537595f082bb69e63c17fe32e5face39e408f7
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-20 18:12:51 +0300

    Frontend - Creating SignupScreen and initializing signup form hook

--- smart-fridge-client/src/features/auth/screens/SignupScreen.js
+++ smart-fridge-client/src/features/auth/screens/SignupScreen.js
@@ -4,3 +4,14 @@ import EmailInput from '../components/EmailInput';
 import PasswordInput from '../components/PasswordInput';
 import { useSignupForm } from '../hooks/useSignupForm';
 import COLORS from '../../../theme/colors';
+const SignupScreen = ({ navigation }) => {
+  const {
+    firstName, setFirstName,
+    lastName, setLastName,
+    email, setEmail,
+    password, setPassword,
+    confirmPassword, setConfirmPassword,
+    passwordVisible, togglePasswordVisibility,
+    confirmVisible, toggleConfirmVisibility,
+    handleSignup,
+  } = useSignupForm(navigation);
