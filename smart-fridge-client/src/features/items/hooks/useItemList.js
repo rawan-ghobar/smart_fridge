7228a8a4eab90188d5bab91e7be785bb58baaa81
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-20 23:02:27 +0300

    Frontend - Importing API service

--- smart-fridge-client/src/features/items/hooks/useItemList.js
+++ smart-fridge-client/src/features/items/hooks/useItemList.js
@@ -3,3 +3,4 @@ import { Alert, Platform, ActionSheetIOS } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import * as ImagePicker from 'expo-image-picker';
 import { useIsFocused } from '@react-navigation/native';
+import api from '../../../services/api';
