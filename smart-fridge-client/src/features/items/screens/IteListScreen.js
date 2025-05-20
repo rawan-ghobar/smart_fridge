ed73971710f4a88e3a0dd878c4bf319357317cdc
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-20 23:22:57 +0300

    Frontend - Importing Ionicons for icons

--- smart-fridge-client/src/features/items/screens/IteListScreen.js
+++ smart-fridge-client/src/features/items/screens/IteListScreen.js
@@ -1,2 +1,3 @@
 import React from 'react';
 import { SafeAreaView, View, Text, TouchableOpacity, FlatList, ActivityIndicator, StyleSheet } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
