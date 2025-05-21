97cdbd7dc8eff8eaecc1e41839e74041dba2b767
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-21 10:45:11 +0300

    Frontend - Importing typography from theme

--- smart-fridge-client/src/features/profile/components/ProfileHeader.js
+++ smart-fridge-client/src/features/profile/components/ProfileHeader.js
@@ -1,3 +1,4 @@
 import React from 'react';
 import { View, Image, Text, StyleSheet } from 'react-native';
 import COLORS from '../../../theme/colors';
+import typography from '../../../theme/typography';
