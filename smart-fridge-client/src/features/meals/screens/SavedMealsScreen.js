9275935a582b09e750340bb0bcf2fe2256bd3a2b
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-21 05:45:35 +0300

    Frontend - Adding saved meals screen

---
+++ smart-fridge-client/src/features/meals/screens/SavedMealsScreen.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { SafeAreaView, View, Text, StyleSheet } from 'react-native';
+import COLORS from '../theme/colors';
+
+const SavedMealsScreen = () => {
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <Text style={styles.text}>Meals Screen</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: COLORS.background,
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: COLORS.primary,
+    fontSize: 24,
+    fontWeight: '600',
+  },
+});
+
+export default SavedMealsScreen;
