9e1c1af2ffae1c032e2c060ac1feaaccbfb3cb12
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-21 10:14:06 +0300

    Frontend - Declaring useNotifications hook

--- smart-fridge-client/src/features/notifications/hooks/useNotifications.js
+++ smart-fridge-client/src/features/notifications/hooks/useNotifications.js
@@ -2,3 +2,4 @@ import { useEffect, useState } from 'react';
 import { Alert } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import api from '../../../services/api';
+const useNotifications = () => {
