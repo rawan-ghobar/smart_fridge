dc3ccab6871aee1d2945f8fa12c19011c80ad168
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-21 10:19:56 +0300

    Frontend - Starting JSX return block

--- smart-fridge-client/src/features/notifications/components/NotificationCard.js
+++ smart-fridge-client/src/features/notifications/components/NotificationCard.js
@@ -2,3 +2,4 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import COLORS from '../../../theme/colors';
 const NotificationCard = ({ message, created_at }) => {
+  return (
