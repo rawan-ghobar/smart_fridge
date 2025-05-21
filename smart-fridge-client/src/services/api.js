cd0a5c5dbd9cb54f3c31c86504a05a56a90479dd
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-21 05:52:47 +0300

    Frontend - Creating axios instance with baseURL and timeout

--- smart-fridge-client/src/services/api.js
+++ smart-fridge-client/src/services/api.js
@@ -1,2 +1,6 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+const api = axios.create({
+  baseURL: 'http://192.168.0.100:8000/api/v0.1',
+  timeout: 10000,
+});
