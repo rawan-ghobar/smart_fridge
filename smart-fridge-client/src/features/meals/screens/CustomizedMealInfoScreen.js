db4f0e7f7b32ef7c864c84d6d1a5f979a7f3e835
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-21 05:04:01 +0300

    Frontend - Importing core react native components

--- smart-fridge-client/src/features/meals/screens/CustomizedMealInfoScreen.js
+++ smart-fridge-client/src/features/meals/screens/CustomizedMealInfoScreen.js
@@ -1,1 +1,17 @@
 import React, { useState } from 'react';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  Alert,
+  ActivityIndicator,
+  KeyboardAvoidingView,
+  ScrollView,
+  Platform,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from 'react-native';
