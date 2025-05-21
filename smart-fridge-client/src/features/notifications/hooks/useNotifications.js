11946b32c3270025de84db6ec3d75d0c15418cbb
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-21 10:14:19 +0300

    Frontend - Setting up notifications state

--- smart-fridge-client/src/features/notifications/hooks/useNotifications.js
+++ smart-fridge-client/src/features/notifications/hooks/useNotifications.js
@@ -3,3 +3,4 @@ import { Alert } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import api from '../../../services/api';
 const useNotifications = () => {
+  const [notifications, setNotifications] = useState([]);
