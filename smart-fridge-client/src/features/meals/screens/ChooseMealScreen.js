2e3ab451f99d91e778745b8fce89a5bd161f5c18
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-21 04:22:37 +0300

    Frontend - Styling header text and image

--- smart-fridge-client/src/features/meals/screens/ChooseMealScreen.js
+++ smart-fridge-client/src/features/meals/screens/ChooseMealScreen.js
@@ -124,3 +124,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     paddingHorizontal: 20,
   },
+  questionText: {
+    ...typography.header,
+    textAlign: 'center',
+    marginTop: 100,
+  },
+  fridgeImage: {
+    width: 200,
+    height: 200,
+    marginTop: 30,
+    marginBottom: 30,
+  },
