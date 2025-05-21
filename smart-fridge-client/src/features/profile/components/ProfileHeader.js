fb481f3d160b54158df0580b79310d44d95dc91b
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-21 10:45:38 +0300

    Frontend - Rendering container view

--- smart-fridge-client/src/features/profile/components/ProfileHeader.js
+++ smart-fridge-client/src/features/profile/components/ProfileHeader.js
@@ -3,3 +3,4 @@ import { View, Image, Text, StyleSheet } from 'react-native';
 import COLORS from '../../../theme/colors';
 import typography from '../../../theme/typography';
 const ProfileHeader = ({ avatar, name, email }) => (
+  <View style={styles.headerContainer}>
