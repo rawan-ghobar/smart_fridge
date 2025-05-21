2067dc05c1c5b478d083de725dd38af58387df44
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-21 05:32:53 +0300

    Frontend - Rendering meal title, image, and calories

--- smart-fridge-client/src/features/meals/screens/MealInfoScreen.js
+++ smart-fridge-client/src/features/meals/screens/MealInfoScreen.js
@@ -19,3 +19,13 @@ return (
     <TouchableOpacity style={styles.backButton} onPress={() => navigation.goBack()}>
       <Ionicons name="arrow-back-outline" size={24} color={COLORS.primary} />
     </TouchableOpacity>
+    <ScrollView contentContainerStyle={styles.container}>
+      <Text style={styles.headerText}>{name}</Text>
+
+      <Image
+        source={require('../../../../assets/fridgeeating.png')}
+        style={styles.mealImage}
+        resizeMode="cover"
+      />
+
+      <Text style={styles.caloriesText}>Total Calories: {calories}</Text>
