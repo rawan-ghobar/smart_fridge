3363f5e1f83a84370e533721ad09514a0411fbd4
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-21 06:21:05 +0300

    Frontend - Rendering Text with custom font and passed props

--- smart-fridge-client/components/textWrapper.js
+++ smart-fridge-client/components/textWrapper.js
@@ -1,3 +1,9 @@
 import React from 'react';
 import { Text } from 'react-native';
 const AppText = ({ style, children, ...props }) => {
+  return (
+    <Text style={[{ fontFamily: 'Outfit-Regular' }, style]} {...props}>
+      {children}
+    </Text>
+  );
+};
