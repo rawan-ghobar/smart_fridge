00ca0d94ff58efee6f2d985e10fa10ce3042635e
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-21 10:19:42 +0300

    Frontend - Declaring NotificationCard component

--- smart-fridge-client/src/features/notifications/components/NotificationCard.js
+++ smart-fridge-client/src/features/notifications/components/NotificationCard.js
@@ -1,3 +1,4 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import COLORS from '../../../theme/colors';
+const NotificationCard = ({ message, created_at }) => {
