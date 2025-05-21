089b102a5b97ae2f25cc98897ff32c77675f81bc
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-21 05:57:31 +0300

    Frontend - Importing auth screens

--- smart-fridge-client/src/routes/routes.js
+++ smart-fridge-client/src/routes/routes.js
@@ -2,3 +2,6 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import BottomTabsRoutes from './BottomTabRoutes';
+
+import LoginScreen from '../features/auth/screens/LoginScreen';
+import SignupScreen from '../features/auth/screens/SignupScreen';
