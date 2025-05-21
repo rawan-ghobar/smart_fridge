d1731bd9ff9520892e7ff3a9fc85fcf069559695
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-21 04:18:36 +0300

    Frontend - Importing API service for meal generation

--- smart-fridge-client/src/features/meals/screens/ChooseMealScreen.js
+++ smart-fridge-client/src/features/meals/screens/ChooseMealScreen.js
@@ -11,3 +11,4 @@ import {
 } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import api from '../../../services/api';
