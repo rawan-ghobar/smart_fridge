59c9fdf1e7d5f342401d53f8024f4ff6b8779c41
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-21 05:59:36 +0300

    Frontend - Adding auth screens to stack navigator

--- smart-fridge-client/src/routes/routes.js
+++ smart-fridge-client/src/routes/routes.js
@@ -23,3 +23,5 @@ const Routes = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Signup" component={SignupScreen} options={{ headerShown: false }} />
