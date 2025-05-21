3f964888f467cf875aa4e38c3fd914c4e3d0c9a9
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-21 10:45:50 +0300

    Frontend - Rendering user avatar image

--- smart-fridge-client/src/features/profile/components/ProfileHeader.js
+++ smart-fridge-client/src/features/profile/components/ProfileHeader.js
@@ -4,3 +4,4 @@ import COLORS from '../../../theme/colors';
 import typography from '../../../theme/typography';
 const ProfileHeader = ({ avatar, name, email }) => (
   <View style={styles.headerContainer}>
+    <Image source={avatar} style={styles.avatar} />
