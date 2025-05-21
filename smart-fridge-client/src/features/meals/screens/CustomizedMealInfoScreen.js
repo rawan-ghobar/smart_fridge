6cf6c52539a28100f348f93ac423e96f37818eb7
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-21 05:27:28 +0300

    Frontend - Defining CustomizedMealInfoScreen and extracting mealData

--- smart-fridge-client/src/features/meals/screens/CustomizedMealInfoScreen.js
+++ smart-fridge-client/src/features/meals/screens/CustomizedMealInfoScreen.js
@@ -11,3 +11,6 @@ import {
 import { Ionicons } from '@expo/vector-icons';
 import COLORS from '../../../theme/colors';
 import typography from '../../../theme/typography';
+const CustomizedMealInfoScreen = ({ route, navigation }) => {
+  const { mealData } = route.params;
+  const { name, usercalories, ingredients, steps } = mealData;
