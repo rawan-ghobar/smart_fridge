c55387d8925a91c8ec90a4917a1d7cbabf4f4f68
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-21 06:37:08 +0300

    Frontend - Handling 'Random Meal' navigation

--- smart-fridge-client/src/features/home/useHomeActions.js
+++ smart-fridge-client/src/features/home/useHomeActions.js
@@ -2,3 +2,6 @@ export const useHomeActions = (navigation) => {
   const userName = 'Rawan';
   const handlePress = (action) => {
     switch (action) {
+      case 'Random Meal':
+        navigation.navigate('ChooseMeal');
+        break;
