7ed267fb552bcb7042bbabc7b8aef8329319a0be
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-21 10:13:54 +0300

    Frontend - Importing API service

--- smart-fridge-client/src/features/notifications/hooks/useNotifications.js
+++ smart-fridge-client/src/features/notifications/hooks/useNotifications.js
@@ -1,3 +1,4 @@
 import { useEffect, useState } from 'react';
 import { Alert } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import api from '../../../services/api';
