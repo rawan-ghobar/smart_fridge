f1069d0e5c0b769811dd65624f15039c5e5ef18f
Rawan Ghobar <rawanghobar@gmail.com>
2025-05-21 05:50:39 +0300

    Frontend - Adding shopping list screen

--- smart-fridge-client/src/features/shoppingLists/screens/ShoppingListScreen.js
+++ smart-fridge-client/src/features/shoppingLists/screens/ShoppingListScreen.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { SafeAreaView, View, Text, StyleSheet } from 'react-native';
-import COLORS from '../theme/colors';
+import COLORS from '../../../theme/colors';
 
-const ShoppingList = () => {
+const ShoppingListScreen = () => {
   return (
     <SafeAreaView style={styles.safeArea}>
       <View style={styles.container}>
@@ -29,4 +29,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default ShoppingList;
+export default ShoppingListScreen;
